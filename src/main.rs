673fdd099bbfb01d8546ba71290feca677b37f7a
armichaud <email@alexandermichaud.tech>
2023-08-18 10:18:05 -0400

    Need to figure out how stack can accommodate multiple types

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,111 @@
 use core::panic;
+use std::process::exit;
 
-enum OPCODES {
-    EXIT,
-    CHICKEN,
-    ADD,
-    SUB,
-    MUL,
-    COMPARE,
-    LOAD,
-    STORE,
-    JUMP,
-    CHAR,
+struct Program {
+    stack: Vec<u8>,
 }
 
-struct Interpreter {
-    stack: [usize; 16],
-}
-
-impl Interpreter {
-    fn parse_and_execute_instruction(&self, n: usize) {
-        if n > 9 {
-            return;
+impl Program {
+    fn new() -> Program {
+        Program {
+            stack: Vec::new(),
         }
-        let opcode = match n {
-            0 => OPCODES::EXIT,
-            1 => OPCODES::CHICKEN,
-            2 => OPCODES::ADD, 
-            3 => OPCODES::SUB,
-            4 => OPCODES::MUL,
-            5 => OPCODES::COMPARE,
-            6 => OPCODES::LOAD,
-            7 => OPCODES::STORE,
-            8 => OPCODES::JUMP,
-            9 => OPCODES::CHAR,
-            _ => panic!("Something went wrong parsing the number of chickens")
-        };
-        self.execute_instruction(opcode);
     }
 
-    fn execute_instruction(&self, opcode: OPCODES) {}
+    fn load_instructions(&mut self, input: String) {
+        for (line_number, line) in input.split("\n").collect::<Vec<&str>>().iter().enumerate() {
+            let mut chicken_count = 0;
+            for symbol in line.split(" ") {
+                if (symbol != "chicken") {
+                    panic!("Invalid instruction '{}', line number {}", symbol, line_number);
+                }
+                chicken_count += 1;
+            }
+            if (chicken_count > 255) {
+                panic!("Overflow error: Chicken count of {} exceeds u8 capacity, line number {}", chicken_count, line_number);
+            }
+            self.stack.push(chicken_count as u8);
+        }
+    }
+
+    fn execute_instruction(&mut self, n: u8) {
+        match n {
+            0 => self.exit(),
+            1 => self.chicken(),
+            2 => self.add(),
+            3 => self.sub(),
+            4 => self.mul(),
+            5 => self.compare(),
+            6 => self.load(),
+            7 => self.store(),
+            8 => self.jump(),
+            9 => self.char(),
+            _ => self.push(n),
+        }
+    }
+
+    fn exit(&self) {
+        exit(0);
+        // TODO 
+    }
+
+    fn chicken(&mut self) {
+        for byte in "Chicken".as_bytes() {
+            self.stack.push(*byte);
+        }
+    }
+
+    fn add(&mut self) {
+        let (a, b) = self.get_top_two_stack_values();
+        a + b;
+    }
+
+    fn sub(&mut self) {
+        let (a, b) = self.get_top_two_stack_values();
+        a - b;
+    }
+
+    fn mul(&mut self) {
+        let (a, b) = self.get_top_two_stack_values();
+        a * b;
+    }
+
+    fn compare(&mut self) {
+        let (a, b) = self.get_top_two_stack_values();
+        if (a == b) {
+            self.stack.push(1 as u8);
+        } else {
+            self.stack.push(0 as u8);
+        }
+    }
+
+    fn load(&self) {
+        println!("TODO");
+    }
+
+    fn store(&self) {
+        println!("TODO");
+    }
+
+    fn jump(&self) {
+        println!("TODO");
+    }
+
+    fn char(&self) {
+        println!("TODO");
+    }
+
+    fn push(&mut self, n: u8) {
+        self.stack.push(n - 10);
+    }
+
+    fn get_top_two_stack_values(&mut self) -> (u8, u8) {
+        let top = self.stack.pop().unwrap(); // TODO: Handle unwrap
+        let second = self.stack.pop().unwrap(); // TODO: Handle unwrap
+        (top, second)
+    }
 }
 
 fn main() {
-    let input = String::from("chicken");
-    let interpreter = Interpreter {
-        stack: [0; 16],
-    };
-
-    for (line_number, line) in input.split("\n").collect::<Vec<&str>>().iter().enumerate() {
-        let mut chicken_count = 0;
-        for symbol in line.split(" ") {
-            if (symbol != "chicken") {
-                panic!("Invalid instruction '{}', line number {}", symbol, line_number);
-            }
-            chicken_count += 1;
-        }
-        interpreter.parse_and_execute_instruction(chicken_count);
-    }
+    let mut program = Program::new();
 }
