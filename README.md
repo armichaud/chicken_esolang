d9b2db7044cdd1144ccbe592666b5e950c418605
armichaud <email@alexandermichaud.tech>
2023-08-21 11:48:47 -0400

    Need to implement load instructions

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # chicken_esolang
 
 [WIP] An  interpreter for the esolang, [chicken](https://esolangs.org/wiki/Chicken), built in Rust.
+
+As described in the wiki documentation, the stack technically begins with the load register set to the address of the stack itself. Implementing this would require the use of RefCells, which seems unnecessary when I can just treat the load register like a vector index.
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 use core::panic;
 use std::process::exit;
 
+const OFFSET: u32 = 2;
+
+#[derive(Debug)]
+enum StackFrame {
+    Token(String),
+    Num(u32)
+}
+
 struct Program {
-    stack: Vec<u8>,
+    stack: Vec<StackFrame>,
 }
 
 impl Program {
-    fn new() -> Program {
+    fn new(user_input: &str) -> Program {
         Program {
-            stack: Vec::new(),
+            stack: Vec::from([StackFrame::Num(0), StackFrame::Token(String::from(user_input))])
         }
     }
 
@@ -21,14 +29,11 @@ impl Program {
                 }
                 chicken_count += 1;
             }
-            if (chicken_count > 255) {
-                panic!("Overflow error: Chicken count of {} exceeds u8 capacity, line number {}", chicken_count, line_number);
-            }
-            self.stack.push(chicken_count as u8);
+            self.stack.push(StackFrame::Num(chicken_count));
         }
     }
 
-    fn execute_instruction(&mut self, n: u8) {
+    fn execute_instruction(&mut self, n: u32) {
         match n {
             0 => self.exit(),
             1 => self.chicken(),
@@ -46,36 +51,47 @@ impl Program {
 
     fn exit(&self) {
         exit(0);
-        // TODO 
     }
 
     fn chicken(&mut self) {
-        for byte in "Chicken".as_bytes() {
-            self.stack.push(*byte);
-        }
+        self.stack.push(StackFrame::Token(String::from("chicken")));
     }
 
     fn add(&mut self) {
         let (a, b) = self.get_top_two_stack_values();
-        a + b;
+        let result = Program::mathematical_operation(a, b, |m, n| m + n ); 
+        // TODO do something with result
     }
 
     fn sub(&mut self) {
         let (a, b) = self.get_top_two_stack_values();
-        a - b;
+        let result = Program::mathematical_operation(a, b, |m, n| m - n ); 
+        // TODO do something with result
     }
 
     fn mul(&mut self) {
         let (a, b) = self.get_top_two_stack_values();
-        a * b;
+        let result = Program::mathematical_operation(a, b, |m, n| m * n );
+        // TODO do something with result
     }
 
     fn compare(&mut self) {
         let (a, b) = self.get_top_two_stack_values();
-        if (a == b) {
-            self.stack.push(1 as u8);
-        } else {
-            self.stack.push(0 as u8);
+        match a {
+            StackFrame::Token(s) => {
+                if let StackFrame::Token(t) = b {
+                    self.stack.push(StackFrame::Num(if s == t { 1 } else { 0 } ));
+                } else {
+                    Program::type_mismatch(StackFrame::Token(s), b);
+                }
+            },
+            StackFrame::Num(m) => {
+                if let StackFrame::Num(n) = b {
+                    self.stack.push(StackFrame::Num(if m == n { 1 } else { 0 } ));
+                } else {
+                    Program::type_mismatch(a,b); 
+                }
+            }
         }
     }
 
@@ -91,21 +107,45 @@ impl Program {
         println!("TODO");
     }
 
-    fn char(&self) {
-        println!("TODO");
+    fn char(&mut self) {
+        let n = self.stack.pop().unwrap();
+        if let StackFrame::Num(n) = n {
+            self.stack.push(StackFrame::Token(String::from(std::char::from_u32(n).unwrap())))
+        } else {
+            self.stack.push(n)
+        }
     }
 
-    fn push(&mut self, n: u8) {
-        self.stack.push(n - 10);
+    fn push(&mut self, n: u32) {
+        self.stack.push(StackFrame::Num(n - 10));
     }
 
-    fn get_top_two_stack_values(&mut self) -> (u8, u8) {
+    fn get_top_two_stack_values(&mut self) -> (StackFrame, StackFrame) {
         let top = self.stack.pop().unwrap(); // TODO: Handle unwrap
         let second = self.stack.pop().unwrap(); // TODO: Handle unwrap
         (top, second)
     }
+
+    fn type_mismatch(a: StackFrame, b:StackFrame) {
+        panic!("Mismatched types, a: {:?}, b: {:?}", a, b)
+    }
+
+    fn mathematical_operation<F: Fn(u32, u32) -> u32>(a: StackFrame, b: StackFrame, operation: F) -> u32 {
+        if let StackFrame::Num(a) = a {
+            if let StackFrame::Num(b) = b {
+                operation(a, b)
+            } else {
+                panic!("Mathematical operation applied to string {:?}", b); 
+            }
+        } else { 
+            panic!("Mathematical operation applied to string {:?}", a); 
+        }
+    }
 }
 
 fn main() {
-    let mut program = Program::new();
+    let input = String::new();
+    let user_input = " ";
+    let mut program = Program::new(user_input);
+    program.load_instructions(input);
 }
