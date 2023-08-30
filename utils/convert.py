35de474744643927edb23cb572b9b2c6d3283358
armichaud <email@alexandermichaud.tech>
2023-08-30 15:53:40 -0400

    Remove util

--- utils/convert.py
+++
@@ -1,9 +0,0 @@
-# Converts .chn to chickens-per-line
-
-with open("[DESTINATION FILENAME]", "r") as file:
-    lines = file.readlines()
-
-with open("[DESTINATION FILENAME]", "w") as file:
-    for line in lines:
-        num_chickens = line.count("chicken")
-        file.write(str(num_chickens) + "\n")
